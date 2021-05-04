f00f0ed74cd058b27e0aaf34d1f87b0c2e988d1f
Ezekiel Ramirez <ezekiel.ramirez@calbaptist.edu>
2021-05-04 16:01:42 -0700

    set up done

--- src/src/main.java
+++ src/src/main.java
@@ -1,2 +1,21 @@
+import java.util.*;
+
+/**
+ * Ezekiel Ramirez
+ * Rounding Variables
+ * Start Date: 5/4/21
+ *
+ * This is a personal project. I want to try to make a program
+ * that rounds any given value to a specified decimal point.
+ *
+ * GIT commands
+ * git add --all
+ * git commit -m "Comment here"
+ * git push
+ */
+
 public class main {
+    public static void main() {
+        //
+    }
 }
